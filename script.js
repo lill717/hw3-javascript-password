83591a32c6a1d0088e9112e374f29666add08db8
Lillian Chan <lilchan717@gmail.com>
2021-06-14 09:32:23 -0500

    Debugged and fixed the var and prompts.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var allCharacters = [];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var c = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -21,43 +21,37 @@ var passwordLength = prompt ("Please choose a number between 8 and 128");
   if (passwordLength < 8 || passwordLength > 128) {
     alert ("The number can only be between 8 and 128");
     generatePassword();
+  }
+
 //how to push lowercaseCharacters array into allCharacters arr  
-  else if (
-    var lowerCase = confirm ("Would you like to add a lowercase letter?"); 
+      var lowerCase = confirm ("Would you like to add a lowercase letter?"); 
       if (lowerCase === true) {
         var passwordText = lowerCaseCharacters.concat(allCharacters); 
       }
-    )
-  } 
+  
 //how to push uppercaseCharacters array into allCharacters arr
-  else if (
     var upperCase = confirm ("Would you like to add a upperCase letter?"); 
       if (upperCase === true) {
         var passwordText = upperCaseCharacters.concat(allCharacters); 
         } 
-      )
+    
   //how to push uppercaseCharacters array into allCharacters arr
-  else if (
-    var numbers = confirm ("Would you like to add a number?"); {
+    var numbers = confirm ("Would you like to add a number?"); 
       if (numbers === true) {
         var passwordText = numbersPass.concat(allCharacters); 
         }
-    )
-  }
+  
  //how to push uppercaseCharacters array into allCharacters arr
-  else if (
   var special = confirm ("Would you like to add a special character?"); 
     if (special === true) {
       var passwordText = specialCharacters.concat(allCharacters); 
       }
-    ) 
   }
   
-
-
+/*
   else {
 //generate the password with allCharacter array with the length user chose
-
+    var generatePassword = allCharacters
 }
 
 }
@@ -68,3 +62,5 @@ var passwordLength = prompt ("Please choose a number between 8 and 128");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+*/
