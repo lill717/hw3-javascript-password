831153e23b5629aa6659a90e4e5e79c9891e33cc
Lillian Chan <lilchan717@gmail.com>
2021-06-16 17:25:58 -0500

    Cleaned up margins, redundant vars, proofread.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,17 +10,15 @@ var result = "";
 
 // Write password to the #password input
 function writePassword() {
-  var passwordArr = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
+  allCharacters = generatePassword();
 }
 
 function generatePassword()  {
   var passwordLength = prompt("Please choose a number between 8 and 128");
-  var passwordArr = [];
   var length = parseInt(passwordLength)
+  // console.log("generate") 
   
   if (length < 8 || length > 128) {
     alert ("The number can only be between 8 and 128");
@@ -30,61 +28,66 @@ function generatePassword()  {
   //how to push lowercaseCharacters array into allCharacters arr  
   var lowerCase = confirm("Would you like to add a lowercase letter?"); 
   if (lowerCase === true) {
-    passwordArr = passwordArr.concat(lowerCaseCharacters); 
+    allCharacters = allCharacters.concat(lowerCaseCharacters); 
   }
   
   //how to push uppercaseCharacters array into allCharacters arr
   var upperCase = confirm("Would you like to add a upperCase letter?"); 
   if (upperCase === true) {
-    passwordArr = passwordArr.concat(upperCaseCharacters); 
+    allCharacters = allCharacters.concat(upperCaseCharacters); 
   } 
     
   //how to push uppercaseCharacters array into allCharacters arr
   var numbers = confirm("Would you like to add a number?"); 
   if (numbers === true) {
-    passwordArr = passwordArr.concat(numbersPass); 
+    allCharacters = allCharacters.concat(numbersPass); 
   }
   
   //how to push uppercaseCharacters array into allCharacters arr
-  var special = confirm("WouldI you like to add a special character?"); 
+  var special = confirm("Would you like to add a special character?"); 
   if (special === true) {
-    passwordArr = passwordArr.concat(specialCharacters); 
+    allCharacters = allCharacters.concat(specialCharacters); 
   }
 
-  function passwordText(allCharacters, passwordLength) {
- 
-    for ( var i = 0; i < passwordLength; i++)  { 
-      result += allCharacters[Math.floor(Math.random() * allCharacters.length) ] ;
-    }
-    
-    function writePassword() {
-      var result = generatedPassword();
-
-      // result = result.replace(“[#password]“, “”);
-      var passwordText.value = document.querySelector(“password”);
-      passwordText.value = result;
-      }
-
-
-
-    // function generatePassword() {
-    //   passwordText = passwordText.replace(password, "");
-    //   // result.value = document.querySelector(“#password”);
-    //   function writePassword() {
-    //     var result = generatePassword();
-    //     result = result.replace[document.querySelector(“#password”)];
-    // }
-
+  for ( var i = 0; i < passwordLength; i++)  { 
+    result += allCharacters[Math.floor(Math.random() * allCharacters.length) ] ;
+  }
+    result = allCharacters.join("");
       console.log(result);
       return result;
-  }
 
-  // this is the array of all characters to choose from in the final password
-  console.log(passwordArr);  
-  passwordText(passwordArr, length);
+      // result();
+      
+  // function passwordText(allCharacters, passwordLength) {
+    // function passwordText(allCharacters, passwordLength) {
+      console.log(passwordText);
+      // var passwordString = allCharacters.join("");
+      // var passwordStr = "";
+    
+
+      // generatePassword();
+
 }
-// document.getElementById(“password”).textContent = “This is your secure password:  ” + passwordText
 
-generatePassword();
+  // return passwordText(allCharacters, length);
 
-    //generate the password with allCharacter array with the length user chose  
+
+
+    
+  //   return allCharacters;
+
+  //   function writePassword() {
+  //     var result = generatedPassword();
+
+  //     passwordText.value = document.querySelector(allCharacters);
+  //     allCharacters.value = result;
+  //     }
+
+
+
+  // generatePassword();
+  
+
+
+//generate the password with allCharacter array with the length user chose 
+
